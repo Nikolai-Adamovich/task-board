daed8285073e605bbe154ea9140bf548fc262712
Mikalai Adamovich <adamovich.nikolai@gmail.com>
2026-08-23 18:23:25 +0200

    fix(ui): move test-setup.ts to project root and fix tsconfig include

--- ui/test-setup.ts
+++ ui/test-setup.ts
@@ -1,6 +1,6 @@
 // Global test setup for Vitest / @angular/build:unit-test
 
-// Polyfill ResizeObserver for jsdom (used by Spartan UI components)
+// Polyfill ResizeObserver for jsdom (used by @spartan-ng/brain internally)
 globalThis.ResizeObserver = class ResizeObserver {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   observe() {}
